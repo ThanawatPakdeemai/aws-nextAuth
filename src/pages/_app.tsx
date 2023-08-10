638a7b3c46699b5af4fa92e9a183b39254826f70
F.Boy <>
2023-08-10 14:36:32 +0700

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,12 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  console.log("test-env-mode", process.env.NEXT_PUBLIC_MODE);
+  console.log("test-env-url", process.env.NEXTAUTH_URL);
+  console.log("test-env-url-public", process.env.NEXT_PUBLIC_MODE);
+  console.log("test-env-id", process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
+  console.log("test-env-secret", process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
