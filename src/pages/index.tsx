6db334b32bdce486b24f51422400e50fbb24725e
F.Boy <>
2023-08-10 17:20:29 +0700

    fix:test set env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
 
   return (
     <>
+      <h2 className="text-center">Mode: {process.env.NEXT_PUBLIC_MODE}</h2>
       <div className="flex gap-4">
         <dia>
           <button
